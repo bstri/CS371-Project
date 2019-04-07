541d4dbaf6af9b09b5984ca630dc8d15bc9a6fa1
bstri <benjamin.stringer@uky.edu>
2019-04-07 17:52:55 -0400

    fixes and adds to networkFlow

--- src/networkFlow.py
+++ src/networkFlow.py
@@ -1,13 +1,40 @@
 from scapy.all import *
+from time import time
 class NetFlow:
-    remoteIP=""
-    protocol=""
-    outgoingPackets=PacketList()
-    incomingPackets=PacketList()
-    #features go here
-    outFPS=0
-    inFPS=0
-    outAvgFrameLength=0
-    inAvgFrameLength=0
-    outDataRate=0
-    inDataRate=0
+
+    # def __init__(self, hostPort, remoteIP, remotePort, transferLayerProtocol):
+    #     self.hostPort = hostPort
+    #     self.remoteIP=remoteIP
+    #     self.protocol=transferLayerProtocol
+    def __init__(self):
+        self.outgoingPackets=PacketList()
+        self.incomingPackets=PacketList()
+
+        #features go here
+        self.outPPS=0
+        self.inPPS=0
+        self.outAvgPacketLength=0
+        self.inAvgPacketLength=0
+        self.outDataRate=0
+        self.inDataRate=0
+
+    def AddIncomingPacket(self, pkt):
+        if not self.incomingPackets: 
+            self.incomingStart = time()
+        else:
+            self.incomingEnd = time()
+        self.incomingPackets.append(pkt)
+
+    def AddOutgoingPacket(self, pkt):
+        if not self.outgoingPackets: 
+            self.outgoingStart = time()
+        else:
+            self.outgoingEnd = time()
+        self.outgoingPackets.append(pkt)
+
+    def GenerateFeatures(self):
+        outgoingTime = self.outgoingEnd - self.outgoingStart
+        outgoingTotalData = sum(map(lambda x: len(x), self.outgoingPackets))
+        self.outDataRate = outgoingTotalData / outgoingTime
+        self.outPPS = len(self.outgoingPackets) / outgoingTime
+        self.outAvgPacketLength = outgoingTotalData / len(self.outgoingPackets)
