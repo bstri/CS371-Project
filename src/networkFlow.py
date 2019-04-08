4f7c1b751f56ddc5b5eec4516924732158c9ff22
Nelson Penn <nelsonapenn@gmail.com>
2019-04-08 19:22:12 -0400

    Fixed overly long line

--- src/networkFlow.py
+++ src/networkFlow.py
@@ -69,5 +69,20 @@ class NetFlow:
         self.totalPackets = len(self.incomingPackets) + len(self.outgoingPackets)
 
     def getCommaSeparatedFeatures(self):
-        attributeList = [self.localPort, self.remoteIP, self.remotePort, self.protocol, self.totalPackets, len(self.incomingPackets), len(self.outgoingPackets), self.totalData, self.inTotalData, self.outTotalData, self.inDataRate, self.outDataRate, self.inPPS, self.outPPS, self.inAvgPacketLength, self.outAvgPacketLength]
+        attributeList = [self.localPort,
+                         self.remoteIP,
+                         self.remotePort,
+                         self.protocol,
+                         self.totalPackets,
+                         len(self.incomingPackets),
+                         len(self.outgoingPackets),
+                         self.totalData,
+                         self.inTotalData,
+                         self.outTotalData,
+                         self.inDataRate,
+                         self.outDataRate,
+                         self.inPPS,
+                         self.outPPS,
+                         self.inAvgPacketLength,
+                         self.outAvgPacketLength]
         return ",".join(map(lambda x: str(x), attributeList))
