1336f63480034e3e5a60e376e6cacb8097ee0fd1
Nelson Penn <nelsonapenn@gmail.com>
2019-04-16 20:56:36 -0400

    Attempt at adding categorical transformation stuff with sklearn_pandas

--- src/mlTraining.py
+++ src/mlTraining.py
@@ -3,15 +3,16 @@ import numpy as np
 import csv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler, LabelBinarizer
+from sklearn.preprocessing import StandardScaler, LabelBinarizer, LabelEncoder
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn import tree
-from sklearn_pandas import DataFrameMapper
+from sklearn_pandas import DataFrameMapper, cross_val_score
 
 df = pd.read_csv("data.csv", header=None)
 # You might not need this next line if you do not care about losing information about flow_id etc. All you actually need to
 # feed your machine learning model are features and output label.
+blacklist = ['remoteIP']  # features not to include
 columns_list = ['localPort',
                 'remoteIP',
                 'remotePort',
@@ -45,11 +46,16 @@ continuous_columns_list = ['totalPackets',
                            'inAvgPacketLength',
                            'outAvgPacketLength']
 df.columns = columns_list
+features = columns_list
+for str in blacklist:
+    categorical_columns_list.remove(str)
+    continuous_columns_list.remove(str)
+    features.remove(str)
+
 dfm = DataFrameMapper(
     [(continuous_col, StandardScaler()) for continuous_col in continuous_columns_list] +
-    [(categorical_col, LabelBinarizer()) for categorical_col in categorical_columns_list]
+    [(categorical_col, LabelEncoder()) for categorical_col in categorical_columns_list]
 )
-features = ['proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
 
 X = df[features]
 y = df['label']
@@ -58,9 +64,12 @@ acc_scores = 0
 for i in range(0, 10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-    #Decision Trees
+    # Decision Trees
     clf = tree.DecisionTreeClassifier()
     clf.fit(X_train, y_train)
+
+    # DataFrameMapper test
+    thing = dfm.fit_transform(X_train, y_train)
 
     # Neural network (MultiPerceptron Classifier)
     # clf = MLPClassifier()
@@ -73,4 +82,7 @@ for i in range(0, 10):
 
 
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
-    result = clf.score(X_test, y_test)  #accuracy score
+    decisionTreeResult = clf.score(X_test, y_test)  #accuracy score
+
+    transformedResult = cross_val_score(thing, X_test, y_test, scoring='accuracy')
+
